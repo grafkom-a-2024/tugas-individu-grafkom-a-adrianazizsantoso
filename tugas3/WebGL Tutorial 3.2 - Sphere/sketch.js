312012e0ce84faa451bef15454a55572bcdcb1bb
adrianazizsantoso <115202624+adrianazizsantoso@users.noreply.github.com>
2024-09-14 12:48:47 +0700

    Add files via upload

---
+++ tugas3/WebGL Tutorial 3.2 - Sphere/sketch.js
@@ -0,0 +1,24 @@
+var img;
+var theta = 0;
+
+function preload() {
+  img = loadImage('UV_Grid_Sm.jpg');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+  background(255, 255, 255, 255);
+
+  rotateZ(theta * mouseX * 0.001);
+  rotateX(theta * mouseX * 0.001);
+  rotateY(theta * mouseX * 0.001);
+
+  //pass image as texture
+  texture(img);
+  sphere(80);
+
+  theta += 0.05;
+}
